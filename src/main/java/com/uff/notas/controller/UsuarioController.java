c714f4c56eb2f69b794781facd20980583c67f18
Artur Melo <arturmelo@id.uff.br>
2021-12-01 11:13:54 -0300

    imports não utilizados removidos

--- src/main/java/com/uff/notas/controller/UsuarioController.java
+++ src/main/java/com/uff/notas/controller/UsuarioController.java
@@ -6,13 +6,8 @@ import com.uff.notas.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/usuario")
@@ -30,15 +25,6 @@ public class UsuarioController {
     public String create(Model model){
         model.addAttribute("usuario", new Usuario());
         return "usuario/new";
-    }
-
-    @PostMapping(value = "/validate")
-    public String validateForm(@RequestBody Usuario usuario, BindingResult result){
-        if(result.hasErrors()){
-            return "usuario/new";
-        }
-        usuarioRepository.save(usuario);
-        return "redirect:/";
     }
 
     
--- src/main/java/com/uff/notas/model/Usuario.java
+++ src/main/java/com/uff/notas/model/Usuario.java
@@ -1,12 +1,12 @@
 package com.uff.notas.model;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -16,7 +16,9 @@ import javax.persistence.Table;
 @Table(name="Usuario")
 public class Usuario {
 
-    @Id @GeneratedValue @Column(name="id")
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long idUsuario;
 
     @Column(name="cpf")
