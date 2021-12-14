26ae210dc427838d6aa4a4f4c9df075b9180d26a
Artur Melo <arturmelo@id.uff.br>
2021-12-14 13:20:43 -0300

    imports de usuarioApi melhorados

--- src/main/java/com/uff/notas/controller/api/UsuarioApi.java
+++ src/main/java/com/uff/notas/controller/api/UsuarioApi.java
@@ -1,23 +1,11 @@
 package com.uff.notas.controller.api;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.uff.notas.model.Usuario;
 import com.uff.notas.repository.UsuarioRepository;
 import com.uff.notas.service.UsuarioService;
 
-import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/usuario")
